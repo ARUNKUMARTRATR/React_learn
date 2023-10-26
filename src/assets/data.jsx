3b2876f74196a49502e7eb38c71726874fed5fce
ARUNKUMARTRATR <arunkumar.tr@experionglobal.com>
2023-10-26 19:01:21 +0530

    data updated

--- src/assets/data.jsx
+++ src/assets/data.jsx
@@ -3,23 +3,28 @@ export const productData = [
     id: 1,
     name: "IPhone 15 Pro Max",
     price: 150000,
-    color: 'Black',
-    img_url: require("./IPhone.jfif")
+    color: "Black",
+    img_url: require("./iphone.jfif"),
   },
   {
     id: 2,
     name: "S23",
     price: 100000,
-    color: 'Graphite',
-    img_url:require('./s23.jpg')
-
+    color: "Graphite",
+    img_url: require("./s23.jpg"),
   },
   {
     id: 3,
     name: "Google Pixel",
     price: 90000,
-    color:'Blue',
-    img_url:require("./google pixel.webp")
-
+    color: "Blue",
+    img_url: require("./google pixel.webp"),
+  },
+  {
+    id: 4,
+    name: "Vivo V29",
+    price: 90000,
+    color: "Saphire",
+    img_url: require("./vivo.webp"),
   },
 ];
